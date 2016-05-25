17f93a53ae33a2165df54fb5d4814a2a7f41ae1f
Виктория Кравченко <vica@MacBook-Pro-Sergey.local>
2016-05-25 16:08:08 +0300

    pxp fix

--- app/source/php/index.php
+++ app/source/php/index.php
@@ -5,11 +5,11 @@
         $last_user_name = htmlspecialchars ($_POST["last_user_name"]);
         $email_user = htmlspecialchars ($_POST["email_user"]);
         $phone_user_number = htmlspecialchars ($_POST["phone_user_number"]);
-        $Message = htmlspecialchars ($_POST["Message"]);
-        $to = 'admin@bitlomanka.top';
-        $subject = " тема сообщения ";
-        if (mail($to, $subject, $Message)) {
-            echo "Message sent";    
+        $message = htmlspecialchars ($_POST["message"]);
+        $to = 'vikaa1190@mail.ru';
+        $subject = "message from the site: http://innosphere.bitlomanka.top/";
+        if (mail($to, $subject, 'Имя пользователя: '.$first_user_name."\r\n". 'Фамилия пользователя: '.$last_user_name."\r\n".'email: '.$email_user."\r\n".'Телефон:' .$phone_user_number."\r\n". 'Сообщение:'.$message)) {
+            echo "message sent";    
         } else {
             echo "Error while sending message";    
         }
